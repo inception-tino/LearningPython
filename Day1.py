94f4188064b09810a10af5f46ce2b4595db1eedd
Tino <x369sage@outlook.com>
2024-07-11 12:11:22 +0100

    Day1 commit

---
+++ Day1.py
@@ -0,0 +1,24 @@
+print("Hello, World!")
+print("How are you doing?")
+print("Don't worry be happy")
+
+"""
+Practice 2
+Display a message that includes your name and your favorite hobby in two different instructions separated by a semicolon. 
+What happens if you write the two print commands in the same line?
+"""
+print("My name is Tino") ;print("Don't worry i play basketball")
+
+"""
+Practice 3
+Fix the following code, keeping the instructions on one line.
+
+print("Welcome to our store.")print("How can I help you?")
+"""
+print("Welcome to our store.");print("How can I help you?")
+
+"""
+Update the following code, putting the instruction on four separate lines.
+print("this is a long string")
+"""
+print("this is \n a \n long \n string")
